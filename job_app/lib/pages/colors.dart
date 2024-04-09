0fe26e105bd69737d951e785f19e6daba1e57a93
sudilvidmal <115799886+sudilvidmal@users.noreply.github.com>
2024-04-09 22:01:49 +0530

    home page
    UI and functionalities of home page

---
+++ job_app/lib/pages/colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class AppColor {
+  static Color homePageBackground = const Color(0xFFfbfcff);
+  static Color gradientFirst = const Color(0xFF0f17ad);
+  static Color gradientSecond = const Color(0xFF6985e8);
+  static Color homePageTitle = const Color(0xFF302f51);
+  static Color homePageSubtitle = const Color(0xFF414160);
+  static Color homePageDetail = const Color(0xFF6588f4);
+  static Color homePageIcons = const Color(0xFF3b3c5c);
+  static Color homePageContainerTextSmall = const Color(0xFFf4f5fd);
+  static Color homePageContainerTextBig = const Color(0xFFffffff);
+  static Color homePagePlanColor = const Color(0xFFa2a2b1);
+  static Color loopColor = const Color(0xFF6d8dea);
+  static Color setsColor = const Color(0xFF9999a9);
+  static Color circuitsColor = const Color(0xFF2f2f51);
+}
